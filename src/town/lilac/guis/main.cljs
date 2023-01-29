a19313ef6fad317a86d7f72355ba1ffee28e0c00
Will Acton <will@willacton.com>
2023-01-29 08:57:09 -0800

    show selected gui

--- src/town/lilac/guis/main.cljs
+++ src/town/lilac/guis/main.cljs
@@ -6,12 +6,13 @@
    [town.lilac.guis.one :as one]
    [town.lilac.guis.two :as two]))
 
-(def selected (flex/source 1))
+(defonce selected (flex/source 1))
 
 (defn menu-item
-  [text on-click]
+  [text on-click selected?]
   (d/li
-   {:class "px-4 py-2 hover:bg-blue-500 hover:text-white cursor-pointer"
+   {:class (str "px-4 py-2 hover:bg-blue-500 hover:text-white cursor-pointer"
+                (when selected? " bg-blue-300"))
     :onclick on-click}
    (d/text text)))
 
@@ -24,19 +25,19 @@
     (d/h1
      {:class "text-xl"}
      (d/text "7 GUIs")))
-   (d/div
-    {:class "flex flex-1"}
+   (fd/track
     (d/div
-     {:class "border-r"}
-     (d/ul
-      (menu-item "Counter" #(selected 1))
-      (menu-item "Temperature converter" #(selected 2))
-      (menu-item "Flight booker" nil)
-      (menu-item "Timer" nil)
-      (menu-item "CRUD" nil)
-      (menu-item "Circle Drawer" nil)
-      (menu-item "Cells" nil)))
-    (fd/track
+     {:class "flex flex-1"}
+     (d/div
+      {:class "border-r"}
+      (d/ul
+       (menu-item "Counter" #(selected 1) (= 1 @selected))
+       (menu-item "Temperature converter" #(selected 2) (= 2 @selected))
+       (menu-item "Flight booker" nil false)
+       (menu-item "Timer" nil false)
+       (menu-item "CRUD" nil false)
+       (menu-item "Circle Drawer" nil false)
+       (menu-item "Cells" nil false)))
      (case @selected
        1 (one/start!)
        2 (two/start!))))))
