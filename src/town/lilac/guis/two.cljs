3e800845f5dc500dc0323fcafb6275f1abcdb2fa
Will Acton <will@willacton.com>
2023-01-29 08:57:16 -0800

    temp converter

--- src/town/lilac/guis/two.cljs
+++ src/town/lilac/guis/two.cljs
@@ -4,18 +4,36 @@
    [town.lilac.flex :as flex]
    [town.lilac.flex.dom :as fd]))
 
-(def counter (flex/source 0))
+(def C (flex/source 0))
+
+(def F (flex/source 32))
+
+(defn update-F!
+  [t]
+  (flex/batch
+   (C (* (- t 32) (/ 5 9)))
+   (F t)))
+
+(defn update-C!
+  [t]
+  (flex/batch
+   (C t)
+   (F (+ 32 (* t (/ 9 5))))))
+
+(defn input
+  [v on-input]
+  (let [el (d/input
+            {:type "number" :value v
+             :oninput #(on-input (js/parseFloat (.. % -target -value)))})]
+    (when (not= (str v) (.-value el))
+      (set! (.-value el) v))))
 
 (defn start!
   []
-  (d/div
-   {:class "flex gap-4 justify-center content-center p-5"}
-   (fd/track
-    (d/div
-     {:class "py-1"}
-     (d/text @counter)))
+  (fd/track
    (d/div
-    (d/button
-     {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded cursor-pointer"
-      :onclick #(counter inc)}
-     (d/text "inc")))))
+    (d/div (input @C update-C!))
+    (d/div (input @F update-F!)))))
+
+(comment
+  (C 32))
