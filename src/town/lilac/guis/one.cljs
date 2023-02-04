2467fec6bd885d38b82d30463078060422fb8f8b
Will Acton <will@willacton.com>
2023-02-04 09:49:46 -0800

    use shared button component in one

--- src/town/lilac/guis/one.cljs
+++ src/town/lilac/guis/one.cljs
@@ -2,7 +2,8 @@
   (:require
    [town.lilac.dom :as d]
    [town.lilac.flex :as flex]
-   [town.lilac.flex.dom :as fd]))
+   [town.lilac.flex.dom :as fd]
+   [town.lilac.guis.lib :as lib]))
 
 (def counter (flex/source 0))
 
@@ -15,7 +16,6 @@
      {:class "py-1"}
      (d/text @counter)))
    (d/div
-    (d/button
-     {:class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded cursor-pointer"
-      :onclick #(counter inc)}
+    (lib/button
+     {:onclick #(counter inc)}
      (d/text "inc")))))
