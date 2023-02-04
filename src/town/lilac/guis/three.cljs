f9387884bc9e3f01aa2e060a25722ebc8d138a01
Will Acton <will@willacton.com>
2023-02-04 10:18:24 -0800

    tightening up flight booker validation

--- src/town/lilac/guis/three.cljs
+++ src/town/lilac/guis/three.cljs
@@ -1,5 +1,6 @@
 (ns town.lilac.guis.three
   (:require
+   [clojure.string :as string]
    [town.lilac.dom :as d]
    [town.lilac.flex :as flex]
    [town.lilac.flex.dom :as fd]
@@ -14,28 +15,46 @@
   []
   (d/form
    {:class "p-5 flex flex-col gap-2"
-    :onsubmit #(.preventDefault %)}
+    :onsubmit (fn [e]
+                (.preventDefault e)
+                (js/alert (pr-str @flight)))}
    (d/select
     {:name "option"
      :onchange #(flight
                  (fn [m]
                    (assoc m :option (keyword (.. % -target -value)))))}
-    (d/option {:value "one-way"} (d/text "one-way flight"))
-    (d/option {:value "return"} (d/text "return flight")))
+    (d/option
+     {:value "one-way"
+      :selected (= :one-way (:option @flight))}
+     (d/text "one-way flight"))
+    (d/option
+     {:value "return"
+      :selected (= :return (:option @flight))}
+     (d/text "return flight")))
    (d/div
     (lib/textbox
-     {:type "text"
+     {:type "date"
+      :class "w-48"
+      :value (:start @flight)
       :oninput #(flight
                  (fn [m]
                    (assoc m :start (.. % -target -value))))}))
    (d/div
     (lib/textbox
-     {:type "text"
-      :disabled (or (= :one-way (:option @flight)) nil)
+     {:type "date"
+      :class "w-48"
+      :value (:return @flight)
+      :disabled (= :one-way (:option @flight))
       :oninput #(flight
                  (fn [m]
                    (assoc m :return (.. % -target -value))))}))
    (lib/button
-    {:onclick #(js/alert "hi")
-     :disabled true}
+    {:disabled (or
+                (and (= :one-way (:option @flight))
+                     (string/blank? (:start @flight)))
+                (and (= :return (:option @flight))
+                     (or (string/blank? (:start @flight))
+                         (string/blank? (:return @flight))
+                         (>= (js/Date. (:start @flight))
+                             (js/Date. (:return @flight))))))}
     (d/text "Book"))))
